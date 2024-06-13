9b473779fb1d3866ababf49a7f4dab185ce251af
Harish Shridhar Nair <57914901+zeal2211@users.noreply.github.com>
2024-06-13 01:34:05 -0500

    Create 143.py

---
+++ 143.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        if not head:
+            return
+        slow = head
+        fast = head 
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        curr = slow
+
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+
+        first, second = head, prev
+
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next            
+        
