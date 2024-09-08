a61aba43f7b2500b20b237e3fe79a4d936b78e40
Harish Shridhar Nair <57914901+zeal2211@users.noreply.github.com>
2024-09-08 03:11:09 -0500

    Create 163.py

---
+++ 163.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+        if not nums:
+            return [[lower, upper]]
+        ans = []
+
+        if nums[0] > lower:
+            ans.append([lower, nums[0] - 1])
+
+        for i in range(1, len(nums)):
+            if nums[i] - nums[i-1] > 1:
+                ans.append([nums[i-1] + 1, nums[i] - 1])
+
+        if nums[-1] < upper:
+            ans.append([nums[-1] + 1, upper])
+
+        return ans
