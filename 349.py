d75e4e49f29ad7f4cb0e66f425a1cb8672fc3036
Harish Shridhar Nair <57914901+zeal2211@users.noreply.github.com>
2024-09-14 01:15:27 -0500

    Create 349.py

---
+++ 349.py
@@ -0,0 +1,12 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashmap = set(nums1)
+        ans = set()
+
+        for num in nums2:
+            if num in hashmap:
+                ans.add(num)
+
+        return list(ans)
+
+        
